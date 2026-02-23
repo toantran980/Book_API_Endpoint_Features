1828324f746671d28a129cc18415e0a08a430344
Toan Tran <65084567+toantran980@users.noreply.github.com>
2026-02-22 17:50:44 -0800

    Refactor Book entity methods to single-line format
    Refactored getter and setter methods for Book entity to use single-line format.

--- SpringBoot/webbackend/src/main/java/com/example/webbackend/entity/Book.java
+++ SpringBoot/webbackend/src/main/java/com/example/webbackend/entity/Book.java
@@ -16,31 +16,19 @@ public class Book {
         this.price = price;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
     public void setId(Long id) {this.id = id;}
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title;}
 
-    public void setTitle(String title) {this.title = title;}
+    public void setTitle(String title) { this.title = title; }
 
-    public String getAuthor() {
-        return author;
-    }
+    public String getAuthor() { return author; }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+    public void setAuthor(String author) { this.author = author; }
 
-    public Double getPrice() {
-        return price;
-    }
+    public Double getPrice() { return price; }
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+    public void setPrice(Double price) { this.price = price; }
 }
